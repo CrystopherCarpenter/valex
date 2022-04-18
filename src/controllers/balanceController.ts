365fb43553095fc33f7d01b0cb50c9307263a34e
Crystopher Carpenter <crystopher.stanley.carpenter@gmail.com>
2022-04-18 11:39:07 -0300

    feat: balance and transactions

--- src/controllers/balanceController.ts
+++ src/controllers/balanceController.ts
@@ -1,10 +1,13 @@
 import { Request, Response } from 'express';
 import * as cardService from '../services/cardService.js';
+import * as balanceService from '../services/balanceService.js';
 
 export async function getBalance(req: Request, res: Response) {
     const { id } = req.params;
 
     await cardService.verifyId(id);
 
-    return res.send();
+    const balance = await balanceService.getData(id);
+
+    return res.send(balance);
 }
--- src/services/balanceService.ts
+++ src/services/balanceService.ts
@@ -1,3 +1,21 @@
-import * as cardRepository from '../repositories/cardRepository.js';
+import * as paymentRepository from '../repositories/paymentRepository.js';
+import * as rechargeRepository from '../repositories/rechargeRepository.js';
 
-export async function newCard() {}
+export async function getData(cardId: any) {
+    const transactions = await paymentRepository.findByCardId(cardId);
+    const recharges = await rechargeRepository.findByCardId(cardId);
+    const balance: number =
+        getTotalAmount(recharges) - getTotalAmount(transactions);
+
+    return { balance, transactions, recharges };
+}
+
+function getTotalAmount(data: any[]) {
+    const amounts = data.map((transaction) => transaction.amount);
+    if (amounts.length === 0) return 0;
+    const totalAmount = amounts.reduce(
+        (current: number, sum: number) => sum + current
+    );
+
+    return totalAmount;
+}
