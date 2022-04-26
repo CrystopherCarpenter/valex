8fedeb0e2759d5e524aedebd344b78e3ce2fb4a3
Liaess <yann.melo@gmail.com>
2022-04-26 12:23:40 -0300

    Preparando revisão de código

--- src/schemas/activationSchema.ts
+++
@@ -1,6 +0,0 @@
-import joi from 'joi';
-
-export const activationSchema = joi.object({
-    CVC: joi.string().min(3).max(3).required(),
-    password: joi.string().pattern(RegExp('^[0-9]{4,4}$')).required(),
-});
--- src/schemas/paymentSchema.ts
+++
@@ -1,7 +0,0 @@
-import joi from 'joi';
-
-export const paymentSchema = joi.object({
-    password: joi.string().pattern(RegExp('^[0-9]{4,4}$')).required(),
-    businessId: joi.number().required(),
-    amount: joi.number().greater(0).required(),
-});
